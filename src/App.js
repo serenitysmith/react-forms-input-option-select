ec2b570d782ac84f4016c5b9da39ac1abf381fcc
serenitysmith <serenitysmith24@gmail.com>
2023-10-04 19:32:33 -0400

    notes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+export default function App() {
+  //  added fave color to our state below as an empty string
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isFriendly: true,
+    employment: "",
+    favColor: "",
+  });
+  console.log(formData.favColor);
 
-function App() {
+  function handleChange(event) {
+    console.log(event);
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+      <textarea
+        value={formData.comments}
+        placeholder="Comments"
+        onChange={handleChange}
+        name="comments"
+      />
+      <input
+        type="checkbox"
+        id="isFriendly"
+        checked={formData.isFriendly}
+        onChange={handleChange}
+        name="isFriendly"
+      />
+      <label htmlFor="isFriendly">Are you friendly?</label>
+      <br />
+      <br />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+      </fieldset>
+      <br />
+
+      <label htmlFor="favColor">What is your favorite color?</label>
+      <br />
+      {/* // added a value here to mirror what we have in state and save the faviorte color  */}
+      <select
+        id="favColor"
+        value={formData.favColor}
+        onChange={handleChange}
+        name="favColor"
+      >
+        <option value="">-- Choose --</option>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="indigo">Indigo</option>
+        <option value="violet">Violet</option>
+      </select>
+    </form>
   );
 }
-
-export default App;
